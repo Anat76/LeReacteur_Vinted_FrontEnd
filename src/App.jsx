89c1d937d3f48d72c3289e9438757d8e0dd408ac
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-03 15:57:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./page/Home";
+import Offer from "./page/offer";
+import Header from "./components/Header";
 
 const App = () => {
-  return <></>;
+  // State qui gere la data reçue
+  const [data, setData] = useState();
+  // State qui permet de savoir si la requete au serveur est bien arrivée
+  const [isLoadind, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        // je stock la reponse dans le state data
+        setData(result.data);
+        console.log(result.data);
+        // je passe mon state isloading en false
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    // j'appel ma fonction fetchData
+    fetchData();
+    // console.log("mon useEffect se render");
+  }, []);
+  // console.log("mon composant est render");
+  return (
+    <>
+      {isLoadind ? (
+        <div>Please wait ...</div>
+      ) : (
+        <>
+          <Router>
+            <Routes>
+              <Header />
+              <Route path="/" element={<Home />} />
+              <Route path="/offer" element={<Offer />} />
+            </Routes>
+          </Router>
+        </>
+      )}
+    </>
+  );
 };
 
 export default App;

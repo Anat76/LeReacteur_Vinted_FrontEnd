7fd7919118d67f6c7a9090147cd84ab28083b9d0
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-03 14:27:12 +0200

    add ReactRouter and Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {

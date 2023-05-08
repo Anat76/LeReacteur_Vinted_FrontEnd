f4301af8c67cfb988aeba0072115b2e0b2b4abdd
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-08 10:00:17 +0200

    Update Login.jsx

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,2 +1,21 @@
-const Login = () => {};
+import { useState } from "react";
+
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newsletter, setNewsletter] = useState(false);
+
+  return (
+    <>
+      <section>
+        <form action="">
+          <input type="text" placeholder="Nom d'utilistateur" />
+          <input type="email" />
+          <input type="password" />
+        </form>
+      </section>
+    </>
+  );
+};
 export default Login;

e91bd0a2ddeb49cba09e485e4d268dca444286b1
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-09 18:58:53 +0200

    finish Publish.jsx

--- src/page/Publish.jsx
+++ src/page/Publish.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-const Publish = () => {
+const Publish = ({ token }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState();
@@ -13,42 +13,128 @@ const Publish = () => {
   const [color, setColor] = useState("");
   const [picture, setPicture] = useState("");
 
-  return (
+  return token ? (
     <>
       <h1>Publier une annonce</h1>
       <form
         onSubmit={async (event) => {
           event.preventDefault();
+          try {
+            const formData = new FormData();
+            formData.append("title", title);
+            formData.append("description", description);
+            formData.append("price", price);
+            formData.append("condition", condition);
+            formData.append("city", city);
+            formData.append("brand", brand);
+            formData.append("size", size);
+            formData.append("color", color);
+            formData.append("picture", picture);
+
+            const response = await axios.post(
+              "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
+              formData,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                  "Content-Type": "multipart/form-data",
+                },
+              }
+            );
+            console.log(response.data);
+          } catch (error) {
+            console.log(error.message);
+          }
         }}
       >
-        <input type="file" />
+        <input
+          type="file"
+          onChange={(event) => {
+            setPicture(event.target.files[0]);
+          }}
+        />
         <div>
           <p>Titre</p>
-          <input type="text" placeholder="ex: Chemise Sezanne Verte" />
-          <p>Decris ton article</p>
           <input
             type="text"
+            placeholder="ex: Chemise Sezanne Verte"
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+            value={title}
+          />
+          <p>Decris ton article</p>
+          <textarea
+            type="text"
             placeholder="ex: Porté quelques fois, taille correctement"
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+            value={description}
           />
         </div>
         <div>
-          <p>marque</p>
-          <input type="text" placeholder="ex: Zara" />
+          <p>Marque</p>
+          <input
+            type="text"
+            placeholder="ex: Zara"
+            onChange={(event) => {
+              setBrand(event.target.value);
+            }}
+            value={brand}
+          />
           <p>Taille</p>
-          <input type="text" placeholder="ex: L / 40 / 12" />
+          <input
+            type="text"
+            placeholder="ex: L / 40 / 12"
+            onChange={(event) => {
+              setSize(event.target.value);
+            }}
+            value={size}
+          />
           <p>Couleur</p>
-          <input type="text" placeholder="ex: Fushia" />
+          <input
+            type="text"
+            placeholder="ex: Fushia"
+            onChange={(event) => {
+              setColor(event.target.value);
+            }}
+            value={color}
+          />
           <p>Etat</p>
-          <input type="text" placeholder="ex: Neuf avec etiquette" />
+          <input
+            type="text"
+            placeholder="ex: Neuf avec etiquette"
+            onChange={(event) => {
+              setCondition(event.target.value);
+            }}
+            value={condition}
+          />
           <p>Lieu</p>
-          <input type="text" placeholder="ex: Paris" />
+          <input
+            type="text"
+            placeholder="ex: Paris"
+            onChange={(event) => {
+              setCity(event.target.value);
+            }}
+            value={city}
+          />
         </div>
         <div>
           <p>Price</p>
-          <input type="text" />
+          <input
+            type="number"
+            onChange={(event) => {
+              setPrice(event.target.value);
+            }}
+            value={price}
+          />
         </div>
+        <input type="submit" value={"Ajouter mon annonce"} />
       </form>
     </>
+  ) : (
+    <Navigate to="/login" />
   );
 };
 
